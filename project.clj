b7e410b7371f72e6ae2c6c13e83e5313c9c4eaf1
Andrey Antukh <niwi@niwi.nz>
2017-03-12 11:49:02 +0100

    Update dev dependencies.

--- project.clj
+++ project.clj
@@ -2,17 +2,21 @@
   :description "File system utilities for clojure"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/Raynes/fs"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+
+  :url "https://github.com/funcool/fs"
+
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.apache.commons/commons-compress "1.13"]
                  [org.tukaani/xz "1.6"]]
 
-  :plugins [[lein-midje "3.1.3"]
-            [codox "0.8.10"]
-            [lein-ancient "0.6.10"]]
-
-  :codox {:src-dir-uri "https://github.com/Raynes/fs/blob/master/"
+  :codox {:src-dir-uri "https://github.com/funcool/fs/blob/master/"
           :src-linenum-anchor-prefix "L"
           :defaults {:doc/format :markdown}}
+
   :deploy-repositories {"releases" :clojars}
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]
+                             [codox "0.10.3"]
+                             [lein-ancient "0.6.10"]]}})
+
+
